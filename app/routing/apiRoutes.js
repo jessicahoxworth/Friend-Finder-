203a6d30bd70f62402a5048175ac5364b935a77c
Jessica Hoxworth <jessicahoxworth@gmail.com>
2020-02-02 17:08:16 -0600

    updated survey route and added alert for unanswered fields

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,57 +1,69 @@
-// ===============================================================================
-// LOAD DATA
-// We are linking our routes to a series of "data" sources.
-// These data sources hold arrays of information on table-data, waitinglist, etc.
-// ===============================================================================
-// var tableData = require("../data/tableData");
 var friends = require("../data/friends");
+
 // ===============================================================================
 // ROUTING
 // ===============================================================================
+
 module.exports = function (app) {
     // API GET Requests
     // Below code handles when users "visit" a page.
     // In each of the below cases when a user visits a link
     // (ex: localhost:PORT/api/admin... they are shown a JSON of the data in the table)
     // ---------------------------------------------------------------------------
+
     app.get("/api/friends", function (req, res) {
-        console.log("Hit route /api/friends")
         res.json(friends);
     });
+
     // API POST Requests
     // Below code handles when a user submits a form and thus submits data to the server.
     // In each of the below cases, when a user submits form data (a JSON object)
     // ...the JSON is pushed to the appropriate JavaScript array
-    // (ex. User fills out a reservation request... this data is then sent to the server...
-    // Then the server saves the data to the tableData array)
     // ---------------------------------------------------------------------------
+
     app.post("/api/friends", function (req, res) {
+        // Note the code here. Our "server" will respond to a user"s survey result
+        // Then compare those results against every user in the database.
+        // It will then calculate the difference between each of the numbers and the user"s numbers.
+        // It will then choose the user with the least differences as the "best friend match."
+        // In the case of multiple users with the same result it will choose the first match.
+        // After the test, it will push the user to the database.
+
+        // We will use this object to hold the "best match". We will constantly update it as we
+        // loop through all of the options
         var bestMatch = {
             name: "",
             photo: "",
-            scores: 10000
-        }
-        var userData = req.body
+            friendDifference: Infinity
+        };
+
+        // Here we take the result of the user"s survey POST and parse it.
+        var userData = req.body;
         var userScores = userData.scores;
 
-        var totalDifference = 0;
+        var totalDifference;
 
         for (var i = 0; i < friends.length; i++) {
-            var thisFriend = friends[i];
+            var currentFriend = friends[i];
             totalDifference = 0;
-            for (var j = 0; j < thisFriend.scores.length; j++) {
-                totalDifference += Math.abs(thisFriend.scores[j] - userScores[j]);
+
+            console.log(currentFriend.name);
+
+            for (var j = 0; j < currentFriend.scores.length; j++) {
+                var currentFriendScore = currentFriend.scores[j];
+                var currentUserScore = userScores[j];
+
+                totalDifference += Math.abs(parseInt(currentUserScore) - parseInt(currentFriendScore));
             }
 
-            if (totalDifference <= bestMatch.scores) {
-                bestMatch.name = thisFriends.name;
-                bestMatch.photo = thisFriend.photo;
-                bestMatch.scores = totalDifference;
+            if (totalDifference <= bestMatch.friendDifference) {
+                bestMatch.name = currentFriend.name;
+                bestMatch.photo = currentFriend.photo;
+                bestMatch.friendDifference = totalDifference;
             }
         }
+
         friends.push(userData);
         res.json(bestMatch);
     });
-
-
 };
